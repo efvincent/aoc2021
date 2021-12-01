e39fc26325d63c6d9b88933a3a04b7b46b177151
Eric Vincent <eric@efvincent.com>
2021-12-01 08:16:16 -0500

    day 01

--- src/AOC/Challenge/Day01.hs
+++ src/AOC/Challenge/Day01.hs
@@ -1,43 +1,46 @@
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
--- |
--- Module      : AOC.Challenge.Day01
--- License     : BSD3
---
--- Stability   : experimental
--- Portability : non-portable
---
--- Day 1.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
-
 module AOC.Challenge.Day01 (
-    -- day01a
-  -- , day01b
+    day01a
+  , day01b
   ) where
 
-import           AOC.Prelude
+import AOC.Prelude ( (:~>)(..) )
 
-day01a :: _ :~> _
+-- | Look at a rolling slice of 2 elements in the array, comparing first
+-- and second, acc a counter whenever second is larger than first 
+solve1 :: [Int] -> Int
+solve1 = go 0
+  where
+    go n (a:b:rest) = if b > a then go (n+1) (b:rest) else go n (b:rest)
+    go n _ = n 
+
+-- | Look at a rolling slice of 3 elements in the list, adding them, and
+-- comparing that to the previous window's sum, accumulating where current
+-- is greater than previous
+solve2 :: [Int] -> Int
+solve2 = go 0 Nothing
+  where
+    go n prv (a:b:c:rest) = 
+      let cur = a+b+c in
+      case prv of
+        Nothing -> go n (Just cur) (b:c:rest)
+        Just p -> if cur > p then go (n+1) (Just cur) (b:c:rest) else go n (Just cur) (b:c:rest)
+    go n _ _ = n
+
+-- Solution wrappers 
+
+day01a :: [Int] :~> Int
 day01a = MkSol
-    { sParse = Just
+    { sParse = Just . map read . lines 
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solve1
     }
 
-day01b :: _ :~> _
+day01b :: [Int] :~> Int
 day01b = MkSol
-    { sParse = Just
+    { sParse = Just . map read . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solve2
     }
