7faf21749c6c8985641c5f482c54b78be6cd128b
Eric Vincent <eric@efvincent.com>
2021-12-02 01:51:56 -0500

    day 2

--- src/AOC/Challenge/Day02.hs
+++ src/AOC/Challenge/Day02.hs
@@ -1,43 +1,47 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
--- |
--- Module      : AOC.Challenge.Day02
--- License     : BSD3
---
--- Stability   : experimental
--- Portability : non-portable
---
--- Day 2.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
+{-# LANGUAGE LambdaCase #-}
 
 module AOC.Challenge.Day02 (
-    -- day02a
-  -- , day02b
+    day02a
+  , day02b
   ) where
 
-import           AOC.Prelude
+import AOC.Prelude ( (:~>)(..) )
+import Data.List.Split (splitOn)
 
-day02a :: _ :~> _
+type Instr = (Char,Int)
+type Pos   = (Int ,Int)
+
+parse :: String -> [Instr]
+parse s = go [] (map (splitOn " ") . lines $ s)
+  where
+    go ins [] = reverse ins
+    go ins ([dir, v]:rest) = go ((head dir, read v):ins) rest
+    go _ _ = error "Bad input"
+
+solve1 :: Pos -> [Instr] -> Int
+solve1 (h,d) = \case 
+  ('f', n):rest -> solve1 (h + n, d)     rest
+  ('d', n):rest -> solve1 (h    , d + n) rest
+  ('u', n):rest -> solve1 (h    , d - n) rest
+  _             -> h * d
+
+solve2 :: Int -> Pos -> [Instr] -> Int
+solve2 a (h,d) = \case
+  ('f', n):rest -> solve2 a       (h + n, d + (a * n)) rest
+  ('d', n):rest -> solve2 (a + n) (h    , d)           rest
+  ('u', n):rest -> solve2 (a - n) (h    , d)           rest
+  _             -> h * d
+
+day02a :: [Instr] :~> Int
 day02a = MkSol
-    { sParse = Just
+    { sParse = Just . parse
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solve1 (0,0)
     }
 
-day02b :: _ :~> _
+day02b :: [Instr] :~> Int
 day02b = MkSol
-    { sParse = Just
+    { sParse = Just . parse
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solve2 0 (0,0)
     }
