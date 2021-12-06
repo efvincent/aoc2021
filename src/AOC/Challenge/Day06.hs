612e8c8099e3ce5d9d2b5a1e8d186e8e4b6537a3
Eric Vincent <eric@efvincent.com>
2021-12-06 04:06:42 -0500

    day 06

--- src/AOC/Challenge/Day06.hs
+++ src/AOC/Challenge/Day06.hs
@@ -1,43 +1,39 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# OPTIONS_GHC -Wno-type-defaults #-}
 
--- |
--- Module      : AOC.Challenge.Day06
--- License     : BSD3
---
--- Stability   : experimental
--- Portability : non-portable
---
--- Day 6.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
+module AOC.Challenge.Day06 (day06a, day06b) where
 
-module AOC.Challenge.Day06 (
-    -- day06a
-  -- , day06b
-  ) where
+import AOC.Solver ( (:~>)(..) )
+import Data.IntMap as IM (insert, empty, (!), elems, delete, mapKeys, updateWithKey)
+import Data.List.Split (splitOn)
 
-import           AOC.Prelude
+solve :: Int -> [Int] -> Int
+solve days nums =
+  go 0 countDowns
+  where
+    countDowns = 
+      let cd = foldl (\acc d -> IM.insert d 0 acc) IM.empty [0..8] in        
+      foldl (\acc d -> IM.insert d ((acc IM.! d)+1) acc) cd nums
+    go day cd 
+      | day < days =
+        let timedOut = cd IM.! 0 in
+        let cd' = IM.insert 8 timedOut 
+                . IM.updateWithKey (\_ v -> Just $ v + timedOut) 6 
+                . IM.mapKeys (subtract 1) 
+                . IM.delete 0 $ cd in
+        go (day+1) cd'
+      | otherwise =
+        sum $ IM.elems cd 
 
-day06a :: _ :~> _
+day06a :: [Int] :~> Int
 day06a = MkSol
-    { sParse = Just
+    { sParse = Just . map read . splitOn ","
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solve 80
     }
 
 day06b :: _ :~> _
 day06b = MkSol
-    { sParse = Just
+    { sParse = Just . map read . splitOn ","
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solve 256
     }
