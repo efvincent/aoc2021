cdad98777723a8a6c69eac4eb9a314e6d6cb9d09
Eric Vincent <eric@efvincent.com>
2021-12-13 10:21:50 -0500

    refactor day 9 in preps for part 2

--- src/AOC/Challenge/Day09.hs
+++ src/AOC/Challenge/Day09.hs
@@ -1,14 +1,40 @@
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module AOC.Challenge.Day09 where -- (day09a, day09b) where
 
 import AOC.Solver ( (:~>)(..) )
-import Data.IntMap as IM (IntMap, (!), fromList, empty, insert, fromList, delete, toList, size)
+import Data.Map as M (Map, (!), fromList, empty, insert, fromList, delete, toList, size, keys)
 import Data.List.Split (splitOn)
 import AOC.Util (strip)
+import AOC.Prelude (traceShowMsg, traceShow, traceShowId)
 
-type Grid = IM.IntMap (IM.IntMap Int)
+data Point = Point
+  { _x :: Int
+  , _y :: Int }
+  deriving stock (Show, Ord, Eq)
+
+data LocState
+  = Unknown
+  | Searching
+  | Low
+  | Basin Point
+  deriving stock (Show)
+
+data Location = Location
+  { _pos :: Point
+  , _state :: LocState
+  , _value :: Int }
+  deriving stock (Show)
+
+instance Ord Location where
+  compare l1 l2 = compare (_pos l1) (_pos l2)
+
+instance Eq Location where
+  (==) l1 l2 = _pos l1 == _pos l2
+
+type Grid = M.Map Point Location
 
 data Puzzle = Puzzle
   { _grid :: Grid
@@ -16,32 +42,58 @@ data Puzzle = Puzzle
   , _maxy :: Int
   } deriving stock (Show)
 
+s :: [Char]
 s = "2199943210\n3987894921\n9856789892\n8767896789\n9899965678"
 
-mkIdx :: [a] -> IntMap a
-mkIdx = IM.fromList . ([0..] `zip`)
+mkIdx :: [a] -> Map Int a
+mkIdx = M.fromList . ([0..] `zip`)
 
-parse :: String -> Puzzle
-parse s =
-  Puzzle g (IM.size (g!0) - 1) (IM.size g - 1)
+solve2 :: Puzzle -> Int
+solve2 p@Puzzle{_grid=g, _maxx=mx, _maxy=my} =
+  0
   where
-    g :: Grid = mkIdx . map (mkIdx . map (read . (:[]))) . lines $ s
-
-get :: Grid -> (Int,Int) -> Int
-get g (x,y) = g!y!x
+    q = 0
 
 solve1 :: Puzzle -> Int
 solve1 Puzzle{_grid=g, _maxx=mx, _maxy=my} =
-  sum . map ((+ 1) . get g) . filter predicate $ idxs
-  where
-    idxs = [(x,y) | x <- [0..mx], y <- [0..my] ]
-    predicate (x,y) =
-      let v = get g (x,y) in
-      let w = x <= 0  || get g (x-1,y) > v in
-      let e = x >= mx || get g (x+1,y) > v in
-      let s = y >= my || get g (x,y+1) > v in
-      let n = y <= 0  || get g (x,y-1) > v in
+  sum . map ((+ 1) . _value . get g) . filter predicate $ idxs
+  where    
+    idxs = [Point x y | x <- [0..mx], y <- [0..my] ]
+    predicate :: Point -> Bool
+    predicate p =
+      let gval = _value . get g in
+      let (x,y) = (_x p, _y p) in
+      let v = gval p in
+      let w = x <= 0  || gval (Point (x-1) y) > v in
+      let e = x >= mx || gval (Point (x+1) y) > v in
+      let s = y >= my || gval (Point x (y+1)) > v in
+      let n = y <= 0  || gval (Point x (y-1)) > v in
       n && s && e && w
+
+parse :: String -> Puzzle
+parse s =
+  let mp = maximum . M.keys $ g in
+  let (mx,my) = (_x mp, _y mp) in
+  Puzzle g mx my
+  where
+    g = M.fromList
+        . map (
+          \(x,(v,y)) ->
+            let p = Point x y in
+            (p, Location p Unknown v))
+        . concatMap (
+          \(y,l) ->
+            [0..] `zip`
+            map ((,y) . subtract 48 . fromEnum) l
+        )
+        . ([0..] `zip`)
+        . lines $ s
+
+
+    -- g :: Grid = mkIdx . map (mkIdx . map (\c -> Location Unknown (fromEnum c - 48))) . lines $ s
+
+get :: Grid -> Point -> Location
+get p coord =  (! coord) p
 
 day09a :: Puzzle :~> Int
 day09a = MkSol
