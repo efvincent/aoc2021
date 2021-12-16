4a49ef99207d49235468bd7761c368c1892b3880
Eric Vincent <eric@efvincent.com>
2021-12-16 10:01:09 -0500

    tidy up day 09

--- src/AOC/Challenge/Day09.hs
+++ src/AOC/Challenge/Day09.hs
@@ -26,13 +26,51 @@ data Puzzle = Puzzle
   , _maxx :: Int
   , _maxy :: Int }
 
+parse :: String -> Puzzle
+parse s =
+  let (mx,my) = maximum . M.keys $ g in
+  Puzzle g mx my
+  where
+    g = M.fromList
+        . map       (\(x,(v,y)) -> ((x,y), Location (x,y) v))
+        . concatMap (\(y,l)     -> [0..] `zip` map ((,y) . subtract 48 . fromEnum) l)
+        . ([0..] `zip`)
+        . lines $ s
+
+-- | filter all indexes down to just those who's neighbors' values are all greater,
+-- then map those points to their values + 1, and sum that list
+solve1 :: Puzzle -> Int
+solve1 Puzzle{_grid=g, _maxx=mx, _maxy=my} =
+  sum . map ((+ 1) . _value . get g) . filter predicate $ idxs
+  where
+    get p coord =  (! coord) p
+    idxs = [(x,y) | x <- [0..mx], y <- [0..my]]
+    predicate p@(x,y) =
+      let gval = _value . get g in
+      let v = gval p in
+      and
+        [ x <= 0  || gval (x-1, y) > v
+        , x >= mx || gval (x+1, y) > v
+        , y >= my || gval (x, y+1) > v
+        , y <= 0  || gval (x, y-1) > v]
+
+-- | perform a flood fill on each point. The flood fill determines (cheaply) if the
+-- point is not fillable, either because it's value is 9 or it has already been filled.
+-- Map the resulting set of @List Location@ (basins) to their lengths, sort them descending,
+-- take the product of the top 3. Note the use of @sortBy (comparing Down)@ is significantly
+-- better performing than @sortOn Down@ - see 
+-- https://ro-che.info/articles/2016-04-02-descending-sort-haskell
 solve2 :: Puzzle -> Int
 solve2 puz@Puzzle{_maxx=mx, _maxy=my} =
+  let idxs = [(x,y) | x <- [0..mx], y <- [0..my] ] in
   let (_, basins) = foldr (floodFill puz) (S.empty, []) idxs in
   product . take 3 . sortBy (comparing Down) . map length $ basins
-  where
-    idxs = [(x,y) | x <- [0..mx], y <- [0..my] ]
 
+-- | collects all the points in a "basin" - effectively the same logic as a simple flood 
+-- fill - by recursively inserting each valid point, and then repeating with each neighbor
+-- of each valid point, until a point has no neighbors. A @Set Point@ is used to track
+-- points that have been seen. In the use of this to determine multiple basins, the set
+-- of seen points is threaded through all the calls to @floodFill@
 floodFill :: Puzzle -> Point -> (Set Point, [[Point]]) -> (Set Point, [[Point]])
 floodFill Puzzle{_grid=g, _maxx=mx, _maxy=my} fillPoint (seen, basins) =
   let (seen', cur) = go fillPoint (seen,S.empty) in
@@ -52,35 +90,6 @@ floodFill Puzzle{_grid=g, _maxx=mx, _maxy=my} fillPoint (seen, basins) =
       let sets = (S.insert pt allFps, S.insert pt fps) in
       let pts = neighbors pt in
       foldr go sets pts
-
-
-solve1 :: Puzzle -> Int
-solve1 Puzzle{_grid=g, _maxx=mx, _maxy=my} =
-  sum . map ((+ 1) . _value . get g) . filter predicate $ idxs
-  where
-    idxs = [(x,y) | x <- [0..mx], y <- [0..my] ]
-    predicate p@(x,y) =
-      let gval = _value . get g in
-      let v = gval p in
-      and
-        [ x <= 0  || gval (x-1, y) > v
-        , x >= mx || gval (x+1, y) > v
-        , y >= my || gval (x, y+1) > v
-        , y <= 0  || gval (x, y-1) > v]
-
-parse :: String -> Puzzle
-parse s =
-  let (mx,my) = maximum . M.keys $ g in
-  Puzzle g mx my
-  where
-    g = M.fromList
-        . map       (\(x,(v,y)) -> ((x,y), Location (x,y) v))
-        . concatMap (\(y,l)     -> [0..] `zip` map ((,y) . subtract 48 . fromEnum) l)
-        . ([0..] `zip`)
-        . lines $ s
-
-get :: Grid -> Point -> Location
-get p coord =  (! coord) p
 
 day9x :: Show a => (Puzzle -> a) -> Puzzle :~> a
 day9x fn = MkSol { sParse = Just . parse, sShow = show, sSolve = Just . fn }
