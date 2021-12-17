11bf99d4ece93ed51b4777538a3bd6c6dfa82639
Eric Vincent <eric@efvincent.com>
2021-12-17 10:47:39 -0500

    clean up days 10 & 11

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -28,7 +28,9 @@ parse =
       '}' -> Close Curly
       '>' -> Close Angle
       c   -> error ("Invalid input character : '" ++ show c ++ "'")
-    
+
+-- | Returns the costs for a token type - first is the error score, second
+-- is the incomplete score for that token type    
 scoreOf :: BType -> (Int,Int)
 scoreOf = \case 
   Round  -> (3,1)
@@ -36,6 +38,12 @@ scoreOf = \case
   Curly  -> (1197,3)
   Angle  -> (25137,4)
 
+-- | Walk the line, handling the different cases. When there's no more input, and
+-- nothing on the stack, then the line has no error. When there's no stack and the 
+-- next input is a close, that's an error. When there's an open followed by a close
+-- of a different token type, that's an error. When there's an open followed by a close,
+-- pop the open off the stack, discard the close, and loop with remaining stack and
+-- input. Finally when the next token is an open, push it on the stack and continue
 errScore :: Line -> Int
 errScore = go []
   where
@@ -45,8 +53,13 @@ errScore = go []
     go ((Open _):tkns) ((Close _):tkns')       = go tkns tkns'
     go stack (open:tkns')                      = go (open:stack) tkns'
 
-incScore :: Line -> Int
-incScore ln =
+-- | If a line has an error, it cannot be checked for incompleteness. Otherwise, 
+-- when there's no more input, incomplete score is determined by remaining opens
+-- on the stack. When there's an open on the stack and a close in the input, pop the 
+-- open off the stack, discard the close, and continue. When there's an open in 
+-- the input, push it on the stack
+incompleteScore :: Line -> Int
+incompleteScore ln =
   case errScore ln of
     0 -> foldl (\acc n -> (acc * 5) + n) 0 (go [] ln)
     _ -> 0 
@@ -55,14 +68,16 @@ incScore ln =
     go ((Open _):tkns) ((Close _):tkns') = go tkns tkns'
     go stack (open:tkns)                 = go (open:stack) tkns
 
+-- | Find the incompete scores for each line and return the median
 solve2 :: [Line] -> Int
 solve2 lns =
-  let scores = sort . filter (/= 0) . map incScore $ lns in
+  let scores = sort . filter (/= 0) . map incompleteScore $ lns in
   (scores !! (length scores `div` 2))    
 
 day10x :: Show a => ([Line] -> a) -> [Line] :~> a
 day10x fn = MkSol { sParse = Just . parse, sShow = show, sSolve = Just . fn}
 
+-- | solution for part 1 is just the sum of the error scores for each line
 day10a :: [Line] :~> Int
 day10a = day10x (sum . map errScore)
 
--- src/AOC/Challenge/Day11.hs
+++ src/AOC/Challenge/Day11.hs
@@ -1,42 +1,28 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
-module AOC.Challenge.Day11 where -- (day11a, day11b) where
+module AOC.Challenge.Day11 (day11a, day11b) where
 
 import AOC.Solver ( (:~>)(..) )
-import Data.Map as M (Map, (!), fromList, keys, adjust, toList, insert)
-import Data.Set as S (Set, empty, insert, toList, member)
-import AOC.Prelude (catMaybes)
-import Data.List (sortBy, insert)
-import Data.Ord (comparing, Down (Down))
-import Data.Maybe ( fromMaybe )
-import Data.List.Split (chunksOf)
+import Data.Map as M (Map, (!), fromList, keys, adjust, insert, elems)
+import Data.Set as S (Set, empty, insert, member)
 
 type Point = (Int,Int)
-type Grid = M.Map Point Int
-
-data Puzzle = Puzzle
-  { _grid :: Grid
-  , _maxx :: Int
-  , _maxy :: Int }
-  deriving stock Show
-
-e = "11111\n19991\n19191\n19991\n11111"
+type Grid = Map Point Int
+type Puzzle = (Grid, Point)
 
 parse :: String -> Puzzle
-parse s =
-  let (mx,my) = maximum . M.keys $ g in
-  Puzzle g mx my
+parse s = (g, maximum . keys $ g)
   where
-    g = M.fromList
+    g = fromList
         . map       (\(y,(v,x)) -> ((x,y), v))
         . concatMap (\(y,l)     -> [0..] `zip` map ((,y) . subtract 48 . fromEnum) l)
         . ([0..] `zip`)
         . lines $ s
 
+-- | returns neighbors of a point, including diagonal, excluding points that 
+-- fall outside of the puzzle
 neighbors :: Puzzle -> Point -> [Point]
-neighbors Puzzle{_maxx=mx, _maxy=my} (x,y) =
+neighbors (_,(mx,my)) (x,y) =
   [(a,b) | a <- [(x-1)..(x+1)]
          , b <- [(y-1)..(y+1)]
          , (a,b) /= (x,y)
@@ -44,66 +30,68 @@ neighbors Puzzle{_maxx=mx, _maxy=my} (x,y) =
          , b >= 0 && b <= my ]
 
 allPoints :: Puzzle -> [Point]
-allPoints Puzzle{_maxx=mx, _maxy=my} =
-  [(x,y) | x <- [0..mx], y <- [0..my]]
+allPoints (_,(mx,my)) = [(x,y) | x <- [0..mx], y <- [0..my]]
 
-pPuz :: Puzzle -> IO ()
-pPuz Puzzle{_grid=g, _maxx=mx} =
-  putStrLn $ unlines . chunksOf (mx+1) . map (toEnum . (+48) . snd) . M.toList $ g  
-
+-- | Runs one generation on the puzzle, returning the number of flashes that
+-- occurred and the new puzzle
 step :: Puzzle -> (Int,Puzzle)
-step puz@Puzzle{_grid=g} =
-  let (flashes, grid) = loop S.empty g' in
-  (flashes, puz { _grid=grid })
+step puz@(g,m) =
+  let (flashes, grid) = go empty steppedGrid in
+  (flashes, (grid,m))
   where
-    g' = (+1) <$> g
+    steppedGrid = (+1) <$> g  -- Increment all values in grid
     allPts = allPoints puz
 
-    flash :: Set Point -> Grid -> Point -> Bool
-    flash fs grid p = (grid!p) > 9 && not (S.member p fs)
+    -- | returns true if the the point has a value > 9 and is not in 
+    -- the set of points that have already flashed this step
+    isFlashing :: Set Point -> Grid -> Point -> Bool
+    isFlashing flashed grid point = (grid!point) > 9 && not (member point flashed)
 
+    -- | Sets all values > 9 to zero
+    zero :: Grid -> Grid
     zero grid = 
-      foldr (\p acc -> 
-        let v = acc!p in 
-        if v <= 9 then acc else M.insert p 0 acc) grid allPts
+      foldr (\p acc -> if acc!p <= 9 then acc else M.insert p 0 acc) grid allPts
 
-    go fs grid [] = (fs,grid)
-    go fs grid (p:pts) | flash fs grid p =
-      let grid' = foldr (M.adjust (+1)) grid (neighbors puz p) in
-      go (S.insert p fs) grid' pts
-    go fs grid (_:pts) = go fs grid pts
+    -- | visit each point, if it is due to flash and hasn't yet, increment all its
+    -- neighbors and add it to the set of points that have flashed this step
+    flashPoints :: Set Point -> Grid -> [Point] -> (Set Point, Grid)
+    flashPoints flashed grid [] = (flashed,grid)
+    flashPoints flashed grid (point:pts) | isFlashing flashed grid point =
+      let grid' = foldr (adjust (+1)) grid (neighbors puz point) in
+      flashPoints (S.insert point flashed) grid' pts
+    flashPoints flashed grid (_:pts) = flashPoints flashed grid pts
 
-    loop fs grid =
-      let (fs', grid') = go fs grid allPts in
-      if length fs' > length fs  
-      then loop fs' grid'
-      else (length fs, zero grid')
+    -- | flash all the points that are due to flash. If nothing flashed, then return
+    -- the accumulated number of flashes and the zero'd out updated grid. Otherwise, 
+    -- accumulate the number of flashes and repeat
+    go :: Set Point -> Grid -> (Int, Grid)
+    go flashed grid =
+      let (flashed', grid') = flashPoints flashed grid allPts in
+      if length flashed' > length flashed  
+      then go flashed' grid'
+      else (length flashed, zero grid')
 
+-- | Run the requested number of steps, accumulating the flashes in each step, returning
+-- the total number of flashes across all steps and the updated puzzle
 steps :: Int -> Puzzle -> (Int, Puzzle)
 steps i p = foldr (\_ (c,puz) -> let (c',puz') = step puz in (c+c', puz')) (0,p) [1..i]
 
-solve1 :: Puzzle -> Int
-solve1 = fst . steps 100
-
+-- | Run a step, if any of the values are non-zero, run another step, otherwise return the
+-- number of steps it took to reach a state where all values are zero
 solve2 :: Puzzle -> Int
 solve2 = go 1
   where
     go n puz =
-      let (_,puz') = step puz in
-      if any ((> 0) . snd) (M.toList (_grid puz'))
+      let (_,puz'@(g,_)) = step puz in
+      if any (> 0) (elems g)
       then go (n+1) puz'
       else n 
 
-day11a :: Puzzle :~> Int
-day11a = MkSol
-  { sParse = Just . parse
-  , sShow  = show
-  , sSolve = Just . solve1
-  }
+day11x :: Show a => (Puzzle -> a) -> Puzzle :~> a
+day11x fn = MkSol { sParse = Just . parse, sShow = show, sSolve = Just . fn }
 
-day11b :: _ :~> _
-day11b = MkSol
-  { sParse = Just . parse
-  , sShow  = show
-  , sSolve = Just . solve2
-  }
+day11a :: Puzzle :~> Int
+day11a = day11x $ fst . steps 100
+
+day11b :: Puzzle :~> Int
+day11b = day11x solve2
