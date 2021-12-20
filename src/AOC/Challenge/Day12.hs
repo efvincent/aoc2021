8402048deaf4e9e160fc4f98a054616130ffcb76
Eric Vincent <eric@efvincent.com>
2021-12-19 20:13:48 -0500

    day 12 wip

--- src/AOC/Challenge/Day12.hs
+++ src/AOC/Challenge/Day12.hs
@@ -1,32 +1,46 @@
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
--- |
--- Module      : AOC.Challenge.Day12
--- License     : BSD3
---
--- Stability   : experimental
--- Portability : non-portable
---
--- Day 12.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
+module AOC.Challenge.Day12 where -- (day12a, day12b) where
 
-module AOC.Challenge.Day12 (
-    -- day12a
-  -- , day12b
-  ) where
+import AOC.Solver ( (:~>)(..) )
+import Data.Map as M (Map, (!), empty, findWithDefault, fromList, keys, adjust, insert, elems)
+import Data.Set as S (Set, toList, empty, insert, member, elems)
+import Data.Data (Data(dataCast2))
+import Data.List.Split (splitOn)
+import Data.Char (isLower)
 
-import           AOC.Prelude
+data CaveType = Start | End | Small | Large deriving stock (Show, Eq, Ord)
+
+data Cave = Cave
+  { _lbl :: String
+  , _type :: CaveType
+  , _nbrs :: Set String }
+  deriving stock (Show)
+
+instance Eq Cave where
+  (==) c1 c2 = _lbl c1 == _lbl c2
+
+instance Ord Cave where
+  compare c1 c2 = compare (_lbl c1) (_lbl c2)
+
+caveType :: String -> CaveType
+caveType "start" = Start
+caveType "end" = End
+caveType s = case s of
+  (c:_) | isLower c -> Small
+  _ -> Large
+
+parse :: String -> Map String Cave
+parse s =
+  foldr (\(a,b) m -> 
+    let c = findWithDefault (Cave a (caveType a) S.empty) a m in
+    let c' = c { _nbrs = S.insert b (_nbrs c) } in
+    M.insert a c' m) M.empty cs2
+  where
+    cs1 = map ((\[a,b] -> (a,b)) . splitOn "-") . lines $ s
+    cs2 = cs1 ++ map (\(a,b) -> (b,a)) cs1
 
 day12a :: _ :~> _
 day12a = MkSol
