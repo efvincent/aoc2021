d44c0cc4cff4afbfc9d35bbe537af48599f25439
Eric Vincent <eric@efvincent.com>
2021-12-22 02:45:04 -0500

    refactor 13

--- src/AOC/Challenge/Day13.hs
+++ src/AOC/Challenge/Day13.hs
@@ -1,13 +1,10 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
-module AOC.Challenge.Day13 where -- (day13a, day13b) where
+module AOC.Challenge.Day13 (day13a, pGrid13, solve2d13) where
 
 import AOC.Solver ( (:~>)(..) )
-import Data.Set as S (Set, fromList, toList, member, empty, insert, union)
+import Data.Set as S (Set, fromList, toList, union)
 import Data.List.Split (splitOn)
-import Data.Char (isLower)
 import Data.List (intercalate)
 import AOC.Util (strip)
 
@@ -19,24 +16,24 @@ type Puzzle = (Grid, Folds)
 
 foldy :: Int -> Grid -> Grid
 foldy fy g =
-  g' `S.union` fpts
+  trimmed `S.union` folded
   where
     gl = toList g
-    fpts = fromList . map (\(px,py) -> (px, fy - (py - fy))) . filter (\(_,py) -> py > fy) $ gl
-    g' = fromList . filter (\(_,py) -> py <= fy) $ gl
+    folded = fromList . map (\(px,py) -> (px, fy - (py - fy))) . filter (\(_,py) -> py > fy) $ gl
+    trimmed = fromList . filter (\(_,py) -> py <= fy) $ gl
 
 foldx :: Int -> Grid -> Grid
 foldx fx g =
-  g' `S.union` fpts
+  trimmed `S.union` folded
   where
     gl = toList g
-    fpts = fromList . map (\(px,py) -> (fx - (px - fx), py)) . filter (\(px,_) -> px > fx) $ gl
-    g' = fromList . filter (\(px,_) -> px <= fx) $ gl
+    folded = fromList . map (\(px,py) -> (fx - (px - fx), py)) . filter (\(px,_) -> px > fx) $ gl
+    trimmed = fromList . filter (\(px,_) -> px <= fx) $ gl
 
 parse :: String -> Puzzle
 parse s =
-  (fromList . map (\str -> let [x,y] = splitOn "," str in (read x, read y)) . splitOn "\n" $ s1
-  ,map (parseFold . drop 11) . splitOn "\n" $ s2)
+  ( fromList . map (\str -> let [x,y] = splitOn "," str in (read x, read y)) . splitOn "\n" $ s1
+  , map (parseFold . drop 11) . splitOn "\n" $ s2)
   where
     [s1, s2] = map strip . splitOn "\n\n" $ s
     parseFold sf =
@@ -52,23 +49,12 @@ maxPt g =
   let my = maximum (map snd pts) in
   (mx,my)
 
-minPt :: Grid -> Point
-minPt g =
-  let pts = toList g in
-  let mx = minimum (map fst pts) in
-  let my = minimum (map snd pts) in
-  (mx,my)
-
-pPuz :: Puzzle -> IO ()
-pPuz (g,_) = pGrid g
-
-pGrid :: Grid -> IO ()
-pGrid g =
+pGrid13 :: Grid -> IO ()
+pGrid13 g =
   putStrLn (intercalate "\n" ls)
   where
     (maxx,maxy) = maxPt g
-    (minx,miny) = minPt g
-    ls = map (\y -> map (\x -> if (x,y) `elem` g then toEnum 9608 else ' ') [minx..maxx]) [miny..maxy]
+    ls = map (\y -> map (\x -> if (x,y) `elem` g then toEnum 9608 else ' ') [0..maxx]) [0..maxy]
 
 solve1 :: Puzzle -> Int
 solve1 (g, f:_)=
@@ -80,6 +66,8 @@ solve1 (g, f:_)=
 solve1 _ = 0
 
 {-
+Solution ... was not going to do character recognition just
+to be able to submit the solution via API
 
 ████ ████ █    ████   ██  ██  ███  ████
 █    █    █    █       █ █  █ █  █ █   
@@ -89,8 +77,10 @@ solve1 _ = 0
 ████ █    ████ █     ██   ███ █  █ █   
 
 -}
-solve2 :: Puzzle -> Grid
-solve2 (g, fs) =
+
+solve2d13 :: String -> Grid
+solve2d13 s =
+  let (g, fs) = parse s in
   foldl (flip fn) g fs
   where
     fn f = case f of
@@ -98,15 +88,4 @@ solve2 (g, fs) =
       Y n -> foldy n
 
 day13a :: Puzzle :~> Int
-day13a = MkSol
-    { sParse = Just . parse
-    , sShow  = show
-    , sSolve = Just . solve1
-    }
-
-day13b :: Puzzle :~> String
-day13b = MkSol
-    { sParse = Just . parse
-    , sShow  = show
-    , sSolve = \_ -> Just "EFLFJGRF"
-    }
+day13a = MkSol { sParse = Just . parse, sShow  = show, sSolve = Just . solve1 }
