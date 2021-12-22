f1a75aecd17f29bdf42ceb5816862bf2ea0ed26c
Eric Vincent <eric@efvincent.com>
2021-12-22 00:17:37 -0500

    day 12

--- src/AOC/Challenge/Day12.hs
+++ src/AOC/Challenge/Day12.hs
@@ -1,15 +1,14 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
-module AOC.Challenge.Day12 where -- (day12a, day12b) where
+module AOC.Challenge.Day12 (day12a, day12b) where
 
 import AOC.Solver ( (:~>)(..) )
-import Data.Map as M (Map, (!), empty, findWithDefault, fromList, keys, adjust, insert, elems)
-import Data.Set as S (Set, toList, empty, insert, member, elems)
-import Data.Data (Data(dataCast2))
+import Data.Map as M (Map, (!), empty, findWithDefault, insert)
+import Data.Set as S (Set, toList, empty, insert)
 import Data.List.Split (splitOn)
 import Data.Char (isLower)
+
+type Caves = Map String Cave
 
 data CaveType = Start | End | Small | Large deriving stock (Show, Eq, Ord)
 
@@ -32,9 +31,9 @@ caveType s = case s of
   (c:_) | isLower c -> Small
   _ -> Large
 
-parse :: String -> Map String Cave
+parse :: String -> Caves
 parse s =
-  foldr (\(a,b) m -> 
+  foldr (\(a,b) m ->
     let c = findWithDefault (Cave a (caveType a) S.empty) a m in
     let c' = c { _nbrs = S.insert b (_nbrs c) } in
     M.insert a c' m) M.empty cs2
@@ -42,16 +41,25 @@ parse s =
     cs1 = map ((\[a,b] -> (a,b)) . splitOn "-") . lines $ s
     cs2 = cs1 ++ map (\(a,b) -> (b,a)) cs1
 
-day12a :: _ :~> _
-day12a = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
-    }
+subPathsToEnd :: String -> Bool -> [String] -> Caves -> [[String]]
+subPathsToEnd curId noDoubleSmall path caves =
+  let cur = caves!curId in
+  concatMap (\subId ->
+    case caveType subId of
+      End -> [subId : path]
+      ct | ct == Start -> []
+      ct | ct == Small && (subId `elem` path) -> 
+        if noDoubleSmall
+        then [] 
+        else subPathsToEnd subId True (subId:path) caves 
+      _ -> subPathsToEnd subId noDoubleSmall (subId : path) caves
+  ) (S.toList . _nbrs $ cur)
 
-day12b :: _ :~> _
-day12b = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
-    }
+day12x :: Show a => (Caves -> a) -> Caves :~> a
+day12x fn = MkSol { sParse = Just . parse, sShow = show, sSolve = Just . fn }
+
+day12a :: Caves :~> Int
+day12a = day12x $ length . subPathsToEnd "start" True ["start"]
+
+day12b :: Caves :~> Int
+day12b = day12x $ length . subPathsToEnd "start" False ["start"]
