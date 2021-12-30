bf6f5c91068bb61607aa30a755f5fc417663d0ba
Eric Vincent <eric@efvincent.com>
2021-12-30 10:08:23 -0500

    day 16 wip

--- src/AOC/Challenge/Day16.hs
+++ src/AOC/Challenge/Day16.hs
@@ -4,8 +4,12 @@
 module AOC.Challenge.Day16 where -- (day16a, day16b) where
 
 import AOC.Solver ( (:~>)(..) )
+import AOC.Common (bToi)
 import Data.Bifunctor ( Bifunctor(second) )
 import qualified Data.Map as M (fromList, keys, (!))
+import Numeric (readHex)
+import Text.Printf (printf)
+
 
 data Packet = Packet 
   { _ver :: Int
@@ -14,7 +18,21 @@ data Packet = Packet
 data Kind = Literal Int
           | Operator Op [Packet]
 
-data Op = Unnknown 
+data Op = Unknown 
+        | Add
+
+hexToBin :: Char -> [Bool]
+hexToBin c =
+  case readHex [c] of
+    (x,_):_ -> map (== '1') . printf "%04b" $ (x::Int)
+    _       -> error "Invalid input"
+
+parse :: String -> Packet
+parse s =
+  Packet 0 0
+  where
+    ver = bToi . concatMap hexToBin . take 3 $ s
+
 
 day16a :: _ :~> _
 day16a = MkSol
