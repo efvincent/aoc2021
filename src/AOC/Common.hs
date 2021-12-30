8ddd5b53c0bfe29c5c1ab6f78920a6337e5e081c
Eric Vincent <eric@efvincent.com>
2021-12-30 14:58:21 -0500

    document common code

--- src/AOC/Common.hs
+++ src/AOC/Common.hs
@@ -14,6 +14,8 @@ module AOC.Common (
   bToi
   ) where
 
--- | converts a list of bools interpreted as a binary number into an Int
+-- | converts a list of bools interpreted as a binary number (ordered from least to 
+-- most significant bit) into an Int. Reverse the list first if ordered from most
+-- significant to least significant bit
 bToi :: [Bool] -> Int
 bToi = foldr (\bit acc -> fromEnum bit + 2 * acc) 0
